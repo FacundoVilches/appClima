25a0b91043c13e6646c961e8238d9f4024d53ff4
Facundo Vilches <facundovilches190@gmail.com>
2022-01-19 21:00:32 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const apiKey = '70fa2745f111d090d17c9c86a6c9a638'
+
+const fetchData = position =>{
+    const {latitude,longitude} = position.coords
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+        .then(response=>response.json())
+        .then(data=> setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data)
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate()
+    }
+
+    Object.keys(weatherData).forEach(key => {
+        document.getElementById(key).textContent = weatherData[key]
+    })
+
+    cleanUp()
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container')
+    let loader = document.getElementById('loader')
+
+    loader.style.display = 'none'
+    container.style.display = 'flex'
+}
+
+const getDate = () => {
+    let date = new Date()
+    return `${date.getDate()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`
+}
+
+const onLoad = () =>{
+    navigator.geolocation.getCurrentPosition(fetchData)
+}
